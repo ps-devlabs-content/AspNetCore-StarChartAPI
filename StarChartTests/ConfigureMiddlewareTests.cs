7c7ff15b27a5600e2c662deebf0b22456e8cb3d4
Jeff Hopper <jeff-hopper@pluralsight.com>
2022-06-09 14:02:53 -0500

    refactored tests for net6.0

--- StarChartTests/ConfigureMiddlewareTests.cs
+++ StarChartTests/ConfigureMiddlewareTests.cs
@@ -6,11 +6,11 @@ namespace StarChartTests
 {
     public class ConfigureMiddlewareTests
     {
-        [Fact(DisplayName = "Call AddMvc In ConfigureServices @call-addmvc-in-configureservices")]
-        public void CallAddMvcInConfigureServicesTest()
+        [Fact(DisplayName = "Call AddControllers In ConfigureServices @call-addmvc-in-configureservices")] //TODO - rename this 
+        public void CallAddControllersInProgramTest()
         {
-            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Startup.cs";
-            Assert.True(File.Exists(filePath), "`Startup.cs` was not found. Did you rename, move, or delete it?");
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Program.cs";
+            Assert.True(File.Exists(filePath), "`Program.cs` was not found. Did you rename, move, or delete it?");
 
             string file;
             using (var streamReader = new StreamReader(filePath))
@@ -18,17 +18,17 @@ namespace StarChartTests
                 file = streamReader.ReadToEnd();
             }
 
-            var pattern = @"services\s*?[.]AddMvc";
+            var pattern = @"builder.Services\s*?[.]AddControllers";
             var rgx = new Regex(pattern);
 
-            Assert.True(rgx.IsMatch(file), "`Startup.ConfigureServices` didn't contain a call for `AddMvc` on `services`");
+            Assert.True(rgx.IsMatch(file), "`Program.cs` didn't contain a call for `AddControllers` on `builder.Services`");
         }
 
         [Fact(DisplayName = "Call AddDbContext In ConfigureServices @call-adddbcontext-in-configureservices")]
-        public void CallAddApplicationDbContextInConfigureServicesTest()
+        public void CallAddApplicationDbContextInProgramTest()
         {
-            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Startup.cs";
-            Assert.True(File.Exists(filePath), "`Startup.cs` was not found. Did you rename, move, or delete it?");
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Program.cs";
+            Assert.True(File.Exists(filePath), "`Program.cs` was not found. Did you rename, move, or delete it?");
 
             string file;
             using (var streamReader = new StreamReader(filePath))
@@ -36,19 +36,19 @@ namespace StarChartTests
                 file = streamReader.ReadToEnd();
             }
 
-            var pattern = @"services\s*?[.]AddDbContext\s*?[<]\s*?ApplicationDbContext\s*?[>]";
+            var pattern = @"builder.Services\s*?[.]AddDbContext\s*?[<]\s*?ApplicationDbContext\s*?[>]";
             var rgx = new Regex(pattern);
 
-            Assert.True(rgx.IsMatch(file), "`Startup.ConfigureServices` didn't contain a call for `AddDbContext` on `services` with the type argument `ApplicationDbContext`");
+            Assert.True(rgx.IsMatch(file), "`Program.cs` didn't contain a call for `AddDbContext` on `builder.Services` with the type argument `ApplicationDbContext`");
             //This doesn't technically verify UseInMemoryDatabase was used as an argument for AddDbContext, however; this will allow for other acceptable ways to handle this task
             Assert.True(file.Contains("UseInMemoryDatabase"), @"`Startup.ConfigureServices` didn't provide `options => options.UseInMemoryDatabase(""StarChart"")` as an argument for `AddDbContext<ApplicationDbContext>`");
         }
 
-        [Fact(DisplayName = "Call UseMvc In Configure @call-usemvc-in-configure")]
-        public void CallUseMvcInConfigureTest()
+        [Fact(DisplayName = "Call MapControllers In Configure @call-usemvc-in-configure")]
+        public void CallMapControllersInProgramTest()
         {
-            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Startup.cs";
-            Assert.True(File.Exists(filePath), "`Startup.cs` was not found. Did you rename, move, or delete it?");
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "StarChart" + Path.DirectorySeparatorChar + "Program.cs";
+            Assert.True(File.Exists(filePath), "`Program.cs` was not found. Did you rename, move, or delete it?");
 
             string file;
             using (var streamReader = new StreamReader(filePath))
@@ -56,10 +56,10 @@ namespace StarChartTests
                 file = streamReader.ReadToEnd();
             }
 
-            var pattern = @"app\s*?[.]UseMvc";
+            var pattern = @"app\s*?[.]MapControllers";
             var rgx = new Regex(pattern);
 
-            Assert.True(rgx.IsMatch(file), "`Startup.Configure` didn't contain a call for `UseMvc` on `app`");
+            Assert.True(rgx.IsMatch(file), "`Program.cs` didn't contain a call for `MapControllers` on `app`");
         }
     }
 }
